10b3f3d9b794cb391fc27dd92b6998570f379901
Sonu64 <sonusantu64@gmail.com>
2025-11-25 09:42:10 +0530

    Update functionProblems.js

--- Frontend/JavaScript/functionProblems.js
+++ Frontend/JavaScript/functionProblems.js
@@ -1,1 +1,31 @@
-console.log("Hello, world !");
+// BMI Calculator Function
+const bmi = (weight, height) => {
+  return weight / height ** 2;
+};
+console.log(bmi(100, 5));
+
+// Greet function with default name
+const greet = (username = "Guest") => {
+  console.log(username);
+  console.log(`Hello, ${username} !`);
+};
+greet();
+greet("Sonu");
+
+// Sum all numbers using REST
+const sumAll = (...nums) => {
+  sum = 0;
+  nums.forEach((num) => {
+    sum += num;
+  });
+  return sum;
+};
+console.log(sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// A function that returns another function -> Higher Order Functions
+const boom = () => {
+  return () => {
+    console.log("Function returned by boom !");
+  };
+};
+boom()();
