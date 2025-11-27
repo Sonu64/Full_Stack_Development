4f58cf84abbe576ee565cf3ac0c49f3a3e673bcf
Sonu64 <sonusantu64@gmail.com>
2025-11-27 07:07:28 +0530

    update arrayProblems.js

--- Frontend/JavaScript/Module1_Functions_Arrays_Objects/objectProblems.js
+++ Frontend/JavaScript/Module1_Functions_Arrays_Objects/objectProblems.js
@@ -9,8 +9,8 @@ const book = {
 console.log(`${book.title}, by ${book.author} costs Rs.${book.price}`);
 console.log(`${book["title"]}, by ${book["author"]} costs Rs.${book["price"]}`);
 
-// Nested Object including user location and address
-const user = {
+// Nested Object including user location and address, Using Optional Chaining
+let user = {
   username: "Sonu",
   location: "West Bengal",
   address: {
@@ -24,7 +24,7 @@ console.log(
 );
 
 // Destructure name and roll from a student object
-const student = {
+let student = {
   studentName: "Sourakanti",
   age: 22,
   roll: 52,
@@ -32,4 +32,40 @@ const student = {
 const { studentName, age } = student; // roll not accessed
 console.log(studentName, age);
 
-// 
+// Loop through keys and values of an object
+console.log(`Book Details -`);
+for (const key in book) {
+  const element = book[key];
+  console.log(`${key.toUpperCase()} : ${element}`);
+}
+
+// Create Array from Object using Object.entires()
+const userDataArray = Object.entries(user);
+console.log(userDataArray);
+
+// Copy an object through Rest Operator -> SHALLOW COPY
+let newStudent = { ...student };
+console.log(newStudent);
+newStudent = {}; // reassigned to empty object
+console.log(student); // remains unchanged
+console.log(newStudent); // empty
+
+// Using JSON.parse(JSON.stringify(objectName)) -> DEEP COPY
+let newUser = JSON.parse(JSON.stringify(user));
+console.log("Copied version initially: ");
+console.log(newUser);
+newUser = { data: "No content found" }; // Changed copied version
+console.log("Copied version after changing: ");
+console.log(newUser);
+console.log("Actual Object is unchanged: ");
+console.log(user);
+
+// Using Computed Properties to access values
+const keyName = "brand";
+const biscuit = {
+  biscuitName: "Marie Lite",
+  [keyName]: "Sunfeast",
+};
+for (const key in biscuit) {
+  console.log(`${key.toUpperCase()}: ${biscuit[key]}`);
+}
