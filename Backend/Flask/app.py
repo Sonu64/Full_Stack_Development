cb5bd05b44f5faba010597382cc41f1700ea2374
Sonu64 <sonusantu64@gmail.com>
2025-11-28 10:16:47 +0530

    added upto URL Processors -> Dynamic URLs

--- Backend/Flask/app.py
+++ Backend/Flask/app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def index():
     return "<h1 style='font-family:\"Century Gothic\";color:magenta;'>First Flask Application</h1>"
 
+
+# URL Processors -> Dynamic URLs
+@app.route("/greet/<username>")
+def greet(username):
+    return f"<h2 style='font-family:\"Century Gothic\";color:red;'>Welcome, {username} !<h2>"
+
+@app.route("/add/<int:a>/<int:b>")
+def add(a, b):
+    return f"<h2 style='font-family:\"Century Gothic\";color:blue;'>{a} + {b} = {a+b}</h2>"
+
+
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5555, debug=True)
