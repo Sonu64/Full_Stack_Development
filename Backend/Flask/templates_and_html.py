f667c36480bd2fd15b3d028ad558a10ae1ec4e23
Sonu64 <sonusantu64@gmail.com>
2025-11-29 17:28:09 +0530

    Started Templates and HTML topics, Dynamic HTML and Jinja2 syntax.

--- Backend/Flask/templates_and_html.py
+++ Backend/Flask/templates_and_html.py
@@ -1,11 +1,14 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='Templates')
 
 @app.route("/")
 def index():
-    return f"Helllo !"
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    
+@app.route("/fruits")
+def printFruitList():
+    fruits = ["Apple", "Orange", "Banana", "Mango", "Pear"]
+    return render_template("fruits.html",fruits = fruits)
+
+if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5555, debug=True)
