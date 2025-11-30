b21f36e708ed7b6aac1374f3b04e9b72178d4298
Sonu64 <sonusantu64@gmail.com>
2025-11-30 12:02:46 +0530

    Template inheritance, base template and filters

--- Backend/Flask/templates_and_html.py
+++ Backend/Flask/templates_and_html.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='Templates')
 
+# Basic Template rendering
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Dynamic HTML
 @app.route("/fruits")
 def printFruitList():
     fruits = ["Apple", "Orange", "Banana", "Mango", "Pear"]
     return render_template("fruits.html",fruits = fruits)
+
+# Filters
+@app.route("/filters")
+def filters():
+    someText = "SouraKanti"
+    return render_template("filters.html", someText=someText)
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5555, debug=True)
