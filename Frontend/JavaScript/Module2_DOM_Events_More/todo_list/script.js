a0025ae16f0bf064152b4e85110c298434629e65
Sonu64 <sonusantu64@gmail.com>
2025-12-11 22:19:12 +0530

    Added Bootstrap Integration in Taskify.io Project, for CS50 assignment - Homepage !

--- Frontend/JavaScript/Module2_DOM_Events_More/todo_list/script.js
+++ Frontend/JavaScript/Module2_DOM_Events_More/todo_list/script.js
@@ -1,6 +1,8 @@
 let uniqueID = parseInt(localStorage.getItem("uniqueID")) || 0;
 
 const renderTasks = (tasksArray) => {
+  if (tasksArray.length === 0)
+    document.getElementById("defaultText").style.display = "block";
   tasksArray.forEach((taskObject) => {
     const taskSection = document.querySelector("#taskListSection");
     // Create div element and put taskName, Done Btn, Edit btn, Delete btn
@@ -13,20 +15,27 @@ const renderTasks = (tasksArray) => {
 
     taskName.innerText = taskObject["name"];
     const doneBtn = document.createElement("button");
-    doneBtn.innerText = "Done";
+    doneBtn.innerText = "✔";
     doneBtn.classList.add("doneBtn");
+    doneBtn.classList.add("btn");
+    doneBtn.classList.add("btn-success");
     const editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
+    editBtn.innerText = "✎";
     editBtn.classList.add("editBtn");
+    editBtn.classList.add("btn");
+    editBtn.classList.add("btn-warning");
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
+    deleteBtn.innerText = "🗑";
     deleteBtn.classList.add("deleteBtn");
+    deleteBtn.classList.add("btn");
+    deleteBtn.classList.add("btn-danger");
     optionsDiv.appendChild(doneBtn);
     optionsDiv.appendChild(editBtn);
     optionsDiv.appendChild(deleteBtn);
     if (taskObject["status"] === "done") {
       taskName.style.textDecoration = "line-through";
-      doneBtn.innerText = "Mark Undone";
+      taskName.style.color = "#aba9a9ff";
+      doneBtn.innerText = "↺";
     }
     taskDiv.appendChild(taskName);
     taskDiv.appendChild(optionsDiv);
@@ -36,6 +45,10 @@ const renderTasks = (tasksArray) => {
 };
 
 const addTask = (t) => {
+  if (t.trim() === "") {
+    alert("Please Add a Task !");
+    return;
+  }
   const taskSection = document.querySelector("#taskListSection");
   document.getElementById("defaultText").style.display = "none";
   // Create div element and put taskName, Done Btn, Edit btn, Delete btn
@@ -48,14 +61,20 @@ const addTask = (t) => {
   const taskName = document.createElement("p");
   taskName.innerText = t;
   const doneBtn = document.createElement("button");
-  doneBtn.innerText = "Done";
+  doneBtn.innerText = "✔";
   doneBtn.classList.add("doneBtn");
+  doneBtn.classList.add("btn");
+  doneBtn.classList.add("btn-success");
   const editBtn = document.createElement("button");
-  editBtn.innerText = "Edit";
+  editBtn.innerText = "✎";
   editBtn.classList.add("editBtn");
+  editBtn.classList.add("btn");
+  editBtn.classList.add("btn-warning");
   const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "Delete";
+  deleteBtn.innerText = "🗑";
   deleteBtn.classList.add("deleteBtn");
+  deleteBtn.classList.add("btn");
+  deleteBtn.classList.add("btn-danger");
   optionsDiv.appendChild(doneBtn);
   optionsDiv.appendChild(editBtn);
   optionsDiv.appendChild(deleteBtn);
@@ -79,10 +98,10 @@ const deleteTask = (uniqueID) => {
     (task) => task["id"] !== parseInt(uniqueID)
   );
   localStorage.setItem("allTasks", JSON.stringify(updatedTasks));
-  if (!JSON.parse(localStorage.getItem("allTasks")))
-    document.getElementById("defaultText").style.display = "none";
+  if (updatedTasks.length === 0)
+    document.getElementById("defaultText").style.display = "block";
   console.log(updatedTasks);
-  alert("Deleting task with id " + uniqueID);
+  //alert("Deleting task with id " + uniqueID);
   const taskToBeDeleted = document.getElementById(uniqueID);
   taskToBeDeleted.style.display = "none";
 };
@@ -101,50 +120,63 @@ const taskDone = (uniqueID) => {
       const taskDiv = document.getElementById(uniqueID);
       const taskParagraph = taskDiv.children[0];
       taskParagraph.style.textDecoration = "line-through";
+      taskParagraph.style.color = "#aba9a9ff";
     } else if (task["id"] === parseInt(uniqueID) && task["status"] === "done") {
       task["status"] = "not done";
       const taskDiv = document.getElementById(uniqueID);
       const taskParagraph = taskDiv.children[0];
       taskParagraph.style.textDecoration = "none";
+      taskParagraph.style.color = "white";
     }
   });
   localStorage.setItem("allTasks", JSON.stringify(tasks));
   console.log(JSON.parse(localStorage.getItem("allTasks")));
 };
 
+let temp = 0;
+
 const showEditSection = (oldTaskName, uniqueID) => {
+  const editForm = document.getElementById("editForm");
   const editSection = document.getElementById("editSection");
+  temp = uniqueID;
   console.log(editSection);
   //const oldTaskName = document.getElementById(uniqueID).children[0].innerText;
-  alert(oldTaskName);
+  //alert(oldTaskName);
+
   editSection.style.display = "block";
+  editSection.classList.add("active");
   document.getElementById("editedTask").value = oldTaskName;
-  const editForm = document.getElementById("editForm");
+
   editForm.addEventListener("submit", (event) => {
     event.preventDefault();
+
     // Changing task name in DOM only
     const newTaskName = document.getElementById("editedTask").value;
-    document.getElementById(uniqueID).children[0].innerText = newTaskName;
+    document.getElementById(temp).children[0].innerText = newTaskName;
+
     // Updating tasks in localStorage
     const tasks = JSON.parse(localStorage.getItem("allTasks"));
     tasks.forEach((task) => {
-      if (task["id"] === parseInt(uniqueID)) {
+      if (task["id"] === parseInt(temp)) {
         task["name"] = newTaskName;
       }
     });
     localStorage.setItem("allTasks", JSON.stringify(tasks));
     console.log(JSON.parse(localStorage.getItem("allTasks")));
     editSection.style.display = "none";
+    editSection.classList.remove("active");
   });
-  //editTask(oldTaskName, uniqueID);
 };
 
-// const editTask = (oldTaskName, uniqueID) => {
-//   //___________________TODO___________________
-
-// };
-
 document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("closeEditSectionBtn")
+    .addEventListener("click", () => {
+      // alert("Closed");
+      editSection.style.display = "none";
+      editSection.classList.remove("active");
+    });
+
   const tasks = JSON.parse(localStorage.getItem("allTasks"));
   let newTaskEdited = "";
   // If tasks doesn't yet exist, create an Empty Array in localStorage
@@ -167,6 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     const t = document.querySelector("#taskBox").value;
     addTask(t);
+    document.querySelector("#taskBox").value = "";
   });
 
   // Delete Task --> EVENT DELEGATION CONCEPT TO BE REVISED !!!
@@ -177,16 +210,14 @@ document.addEventListener("DOMContentLoaded", () => {
       deleteTask(event.target.parentElement.parentElement.id);
     } else if (event.target.classList.contains("doneBtn")) {
       console.log(event.target.innerText);
-      if (event.target.innerText === "Done")
-        event.target.innerText = "Mark Undone";
-      else if (event.target.innerText === "Mark Undone")
-        event.target.innerText = "Done";
+      if (event.target.innerText === "✔") event.target.innerText = "↺";
+      else if (event.target.innerText === "↺") event.target.innerText = "✔";
       taskDone(event.target.parentElement.parentElement.id);
       console.log(
         "Done task with ID: " + event.target.parentElement.parentElement.id
       );
     } else if (event.target.classList.contains("editBtn")) {
-      //alert("Editing " + event.target.parentElement.parentElement.id);
+      //alert("Edit Button clicked !");
       showEditSection(
         event.target.parentElement.parentElement.children[0].innerText,
         event.target.parentElement.parentElement.id
