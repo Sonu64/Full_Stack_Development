15fd75e171a0f260357d25064afcaa096ae205df
Sonu64 <sonusantu64@gmail.com>
2026-03-19 19:50:37 +0530

    Add custom React element creation example in main.jsx

--- Frontend/React/01_React_Basics-vite/src/main.jsx
+++ Frontend/React/01_React_Basics-vite/src/main.jsx
@@ -2,8 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import MyName from './MyName.jsx'
+import React from 'react'
+
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click Me to Visit Google !",
+};
+
+const actualReactElement = React.createElement(
+  // tagname goes first
+  "a",
+  // then goes the props object
+  {href:"https://google.com", target:"_blank", style: { color: "red" }},
+  // Direct text to be displayed goes next 
+  "Click Me to Visit Google !"
+)
 
 
 createRoot(document.getElementById("root")).render(
-  App() // After all App is a function, so they can be called !
+  // After all App is a function, so they can be called !
+  // Can we put React Element here ? After all we are saving a step of the bundler by directly passinng the tree-like struct.
+  // reactElement But this gives error, and does not work !
+  // this is because in reactElement we created the object according to our own naming conventions, this will not be allowed by react !! 
+  
+  // To create an Element by our own Tree-like Structure, we have to use React.createElement and follow the object key naming conventions used by react !
+  // Now we can put the actualReactElement directly here ! as it follows the object naming convention used actually by react, achieved by React.createElement
+  actualReactElement
 );
